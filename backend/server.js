60e6355c7f8328b6d3953459478037b15bcb37e7
Srujangupta06 <srujan93811@gmail.com>
2025-07-06 09:49:42 +0530

    modified cors config in backend

--- backend/server.js
+++ backend/server.js
@@ -17,13 +17,7 @@ const allowedOrigins = [process.env.FRONTEND_DEV_URL, process.env.FRONTEND_URL];
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: allowedOrigins,
     methods: ["GET", "POST"],
     credentials: true,
   })
